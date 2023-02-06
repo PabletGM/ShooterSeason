f61fed9ae3a565f4da5a1045a315839fbb8e8f77
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-02-06 20:38:01 +0100

    Camara de portal para ver al otro lado , el problema es que se mueve contigo mientras caminas
    Posee el fallo de que parece que esta conectada a la mainCamera y su movimiento

---
+++ Assets/PortalCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+
+    #region references
+    /// <summary>
+    /// reference to player camera
+    /// </summary>
+    public Transform playerCamera;
+    /// <summary>
+    /// reference to portal where the camera is
+    /// </summary>
+    public Transform portal;
+    /// <summary>
+    /// reference to otherPortal where you are looking to
+    /// </summary>
+    public Transform otherPortal;
+
+    #endregion
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distancia del jugador a el portal = posicion de camara - posicion otro portal
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        //posicion de la camara = posicion del portal + offset
+        transform.position = portal.position + playerOffsetFromPortal;
+
+
+        //devuelve angulo de rotacion entre varios portales
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        //devuelve rotacion entre portales
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        //nueva direccion de  camara
+        Vector3 newCamaraDirection = portalRotationalDifference * playerCamera.forward;
+        //transformamos esta direcion calculada en rotacion
+        transform.rotation = Quaternion.LookRotation(newCamaraDirection,Vector3.up);
+    }
+}
---
+++ Assets/PortalTextureSetUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetUp : MonoBehaviour
+{
+    //script que pone las dimensiones del render Texture al de la pantalla actual
+
+    #region references
+       public Camera cameraRenderPortalPueblo;
+       public Material cameraMaterial;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        //si existe la camara
+        if(cameraRenderPortalPueblo.targetTexture!=null)
+        {
+            cameraRenderPortalPueblo.targetTexture.Release();
+        }
+        //ponemos tamaño de la renderTexture y el depth=24
+        cameraRenderPortalPueblo.targetTexture = new RenderTexture(Screen.width, Screen.height, 20);
+        cameraMaterial.mainTexture = cameraRenderPortalPueblo.targetTexture;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
