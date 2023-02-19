2a375efbd1763c30fba136454cbff831f4d149bd
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-02-19 23:46:00 +0100

    Cinemática puesta y quitada correctamente devolviendo permisos a Player

--- Assets/Scripts/Cinematicas/EjecutorCinematicaCuernos/EjecutorCinematicaCuernos.cs
+++ Assets/Scripts/Cinematicas/EjecutorCinematicaCuernos/EjecutorCinematicaCuernos.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EjecutorCinematicaCuernos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //creamos un evento
+    [SerializeField]
+    private UnityEvent evento;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Player")
+        {
+            //si se choca el player invocamos evento
+            evento.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HabilitarCursor()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
