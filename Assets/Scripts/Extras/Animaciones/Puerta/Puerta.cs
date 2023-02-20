955ff1e2913f1c02a1aeb74733f24d42de85104b
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-02-21 00:20:46 +0100

    Puerta o Valla con animacion de cerrar o salir al detectar al player en Templo

---
+++ Assets/Scripts/Extras/Animaciones/Puerta/Puerta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    [SerializeField]
+    private protected Animator puerta;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            puerta.Play("Abrir");
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            puerta.Play("Cerrar");
+        }
+       
+    }
+}
