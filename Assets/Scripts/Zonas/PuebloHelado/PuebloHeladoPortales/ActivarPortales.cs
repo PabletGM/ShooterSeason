36d38457a7981674489e29f00dae83d8f43d23e3
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-02-23 21:41:58 +0100

    Portales funcionales y listos para usar

---
+++ Assets/Scripts/Zonas/PuebloHelado/PuebloHeladoPortales/ActivarPortales.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivarPortales : MonoBehaviour
+{
+    //creamos un evento
+    [SerializeField]
+    private protected UnityEvent eventoPortales;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //hacer evento que haga inicio de portales
+        //si se choca el player invocamos evento
+        //este evento en cada portal---> quita efecto de particulas y desactiva TeletransporteCollide
+        //desactiva icono cerrado candado y pone candado abierto
+        eventoPortales.Invoke();
+    }
+}
