bf1ce7ea6805a01f2e079c70fbbad57209d7e3cf
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-02-26 21:59:06 +0100

    Nuevo Package de VFX

---
+++ Assets/Scripts/Zonas/IslaHielo/ColliderArribaMontaña/MisionArribaMontaña.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisionArribaMontaña : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        LogicaObjetivosTemplo.GetInstance().SetNewMission("VInvestiga la zona");
+        //activamos efecto de particulas de humo
+
+        //desactivamos este gameObject
+    }
+}
