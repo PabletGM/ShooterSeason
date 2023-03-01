fe44dcf8dbab7bcd421ce226d4d901130e236ac0
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-03-01 17:52:01 +0100

    Comprobamos en el script teletransporte que va a IslaHielo
    Para aplica el evento de activar cinematica con el trigger

--- Assets/Scripts/Extras/PowerUp/Teletransporte/Teletransporte.cs
+++ Assets/Scripts/Extras/PowerUp/Teletransporte/Teletransporte.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Teletransporte : MonoBehaviour
 {
@@ -11,9 +12,22 @@ public class Teletransporte : MonoBehaviour
     [SerializeField]
     private protected GameObject ThePlayer;
 
+    [SerializeField]
+    private protected UnityEvent eventoCambioIsla;
+
+    private bool permitirCinematica = true;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("cogido");
+        //comprobamos si es el portal a la islaHielo para poner cinematica
+        if(permitirCinematica && this.gameObject.transform.parent.name == "puertaTeleportCinematica")
+        {
+            //solo se hace 1 vez
+            eventoCambioIsla.Invoke();
+            permitirCinematica = false;
+        }
+        
         //la  posicion del jugador pasa a ser la del target
         ThePlayer.transform.position = Target.transform.position;
         //si estamos en la puerta del inicio y vamos a la del pueblo 2 desconectamos temporalmente el trigger de la otra para poder teletransportarnos
